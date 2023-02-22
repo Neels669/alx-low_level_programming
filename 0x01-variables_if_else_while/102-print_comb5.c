f38839283410771d801b7ed537c7cd8b6d5b26c1
Neelam <neelam_669@yahoo.com>
2023-02-22 04:35:46 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int no1, no2;
 
-		for (no1 = 0; no1 >= 9; no1++)
+		for (no1 = 0; no1 <= 9; no1++)
 		{
-			for (no2 = 0; no2 >= 9; no2++)
+			for (no2 = 0; no2 <= 9; no2++)
 			{
 				putchar((no1 % 10) + '0');
 				putchar((no2 % 10) + '0');
