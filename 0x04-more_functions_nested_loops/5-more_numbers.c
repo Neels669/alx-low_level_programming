b07e5be13d3cc907241e458f62c0f67c8ccb30e5
Neelam <neelam_669@yahoo.com>
2023-02-23 11:38:54 -0800

    Task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to print ten times the numbers (0to14)
+ *
+ * Return: Succes (0)
+ *
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
