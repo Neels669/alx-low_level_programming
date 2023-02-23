fe3ae65a842d62a07204c5e87b615ef9df7c5ea5
Neelam <neelam_669@yahoo.com>
2023-02-23 11:04:08 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to check for digit 0 to 9
+ *
+ * Return: Success (0)
+ *
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
