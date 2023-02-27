ff57b267dde16afe1d7d27ef68257b7c33dc7359
Neelam <neelam_669@yahoo.com>
2023-02-27 05:56:15 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Function to print a string in reverse
+ * @s: data type character for the string
+ *
+ * Result: void
+ *
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
+}
