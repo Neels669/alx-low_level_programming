04e66ed63ebfc098b26d8b9c750f9c8afbddb947
Neelam <neelam_669@yahoo.com>
2023-06-12 12:33:15 -0700

    Task 4

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]);
 int main(int argc, char __attribute__((unused)) *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char **argv);
+int main(int argc, char **argv);
 
 int _putchar(char c);
 
