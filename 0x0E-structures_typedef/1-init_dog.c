77069631da4b046fbf262f761a9695b06ab0d636
Neelam <neelam_669@yahoo.com>
2023-06-19 12:21:48 -0700

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+
 /**
  * init_dog - cinitialize a dog data structure.
  * @d: dog pointer.
@@ -7,8 +8,11 @@
  * @owner: dog's owner
  *
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
