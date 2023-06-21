2841d974c056604c53df315254b8d6c87c3ca591
Neelam <neelam_669@yahoo.com>
2023-06-21 05:56:50 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ *
+ * @name - Pointer to the name
+ * @f - Pointer to the printing function
+ *
+ * Return - Name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == null)
+		f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,12 @@
-#ifdef HEAD_
-#define HEAD_
+#ifndef HEAD_FUN_POINT
+#define HEAD_FUN_POINT
+
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
 
 #endif
