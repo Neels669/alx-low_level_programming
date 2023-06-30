329dc57fa6c756701677f4e0810d7004a580f4bf
Neelam <neelam_669@yahoo.com>
2023-06-30 12:11:10 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *current;
-	size_t c;
+	unsigned int count = 0;
 
-	c = 0;
-	current = h;
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", current->len, current->str);
-		current = current->next;
-		c++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
-
-	return (c);
+	return (count);
 }
