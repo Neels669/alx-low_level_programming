858d982851abe77f2aaf20952885b5e0f19e86de
Neelam <neelam_669@yahoo.com>
2023-07-04 05:05:25 -0700

    Amend

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,6 @@
  * print_listint - function with one argument
  * @h: pointer to struct
  *
- * Description: prints all the elements of a list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
